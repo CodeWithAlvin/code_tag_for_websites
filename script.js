c56cc56a849fa40bb9e4c5fa77fbcb23478489d8
Alvin saini <codewithalvin@gmail.com>
2021-05-29 10:34:14 +0530

    V1 completed

---
+++ script.js
@@ -0,0 +1,34 @@
+//getting all elements with tag <code>
+code = document.getElementsByTagName('code')
+
+//changes all tags with <code> to make them in div and pre tags
+const manupulate=(collection)=>{
+    Array.from(collection).map((ele)=>{
+        text=ele.textContent
+        ele.innerHTML=`<div class="container" >
+            <div class="code">
+           <button class="copy" onClick=copy(this) >Copy</button>
+          <pre class="pre">${text}</pre>
+      </div>
+      </div>`
+        
+    })
+}
+
+//calling function
+manupulate(code)
+
+//change text to copied for some time and then reset to copy
+const copiedEffect=(element)=>{
+    element.innerText='Copied !!'
+    setTimeout(()=>element.innerText='Copy',450);
+}
+
+// function called on clicking the copy button
+const copy=(e)=>{
+    text=e.offsetParent.innerText
+    text=text.slice(5,)
+    const cb = navigator.clipboard;
+    cb.writeText(text).then(() => copiedEffect(e));
+}
+
