09b732f2805aa3fcc4ead97a673fc9a5e31d5493
Alvin saini <codewithalvin@gmail.com>
2021-05-31 13:48:56 +0530

    Too many changes
    1) Theme introduced
    2) used svg instead of buttons
    3) used diffrent strategy to select the copy the text
    4) better colors used gradients
    5) mention how to overwrite theme

--- src/script.js
+++ src/script.js
@@ -1,37 +1,60 @@
 //changes all tags with <code> to make them in div and pre tags
 const manupulate=(collection)=>{
     Array.from(collection).map((ele)=>{
-        text=ele.textContent
+        text=ele.textContent;
         ele.innerHTML=`<div class="container-code" >
-            <div class="code-code">
-           <button class="copy-code" onClick=copy(this) >Copy</button>
+            <div class="code-code dark_code">
+            <div class="controls-code dark_nav">
+                <img class="svg" src="src/svg/code.svg">
+                <img class="svg night" id="theme" onClick=theme(this) src="src/svg/sun.svg">
+                <img class="svg" id="copy" onClick=copy(this) src="src/svg/clipboard.svg">
+            </div>   
           <pre class="pre-code"></pre>
       </div>
       </div>`;
-      ele.querySelector(".pre-code").innerText=text
+      ele.querySelector(".pre-code").textContent=text;
     })
 }
 
 
 //change text to copied for some time and then reset to copy
 const copiedEffect=(element)=>{
-    element.innerText='Copied !!'
-    setTimeout(()=>element.innerText='Copy',450);
+    element.src="/src/svg/check.svg";
+    setTimeout(()=>element.src='/src/svg/clipboard.svg',450);
 }
 
 // function called on clicking the copy button
 const copy=(e)=>{
-    text=e.offsetParent.innerText
-    text=text.slice(5,)
+    let text=e.parentElement.parentElement.querySelector(".pre-code").textContent;
     const cb = navigator.clipboard;
     cb.writeText(text).then(() => copiedEffect(e));
 }
+
+const handleTheme=(element)=>{
+    element.querySelector(".controls-code").classList.toggle("light_nav");
+    element.classList.toggle("light_code");
+    element.querySelector(".controls-code").classList.toggle("dark_nav");
+    element.classList.toggle("dark_code");
+    element.querySelector("#theme").classList.toggle("night")
+    if (element.querySelector("#theme").classList.contains("night")){
+    element.querySelector("#theme").src="/src/svg/sun.svg"
+    }
+    else{
+    element.querySelector("#theme").src="/src/svg/moon.svg"
+    }
+}
+
+const theme=(e)=>{
+    elements=document.getElementsByClassName("code-code");
+    Array.from(elements).map((element)=>handleTheme(element));
+};
+
 
 //calling function
 //getting all elements with tag <code>
 //window.onload to prevent run js without html if js fetched in head
 window.onload=()=>{
-    code = document.getElementsByTagName('code')
-    manupulate(code)
-}
+    code = document.getElementsByTagName('code');
+    manupulate(code);
+};
 
